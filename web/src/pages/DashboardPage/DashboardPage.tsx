afec165c142ba83e40a5bcdc181a9878fc4a841f
Mohammed Abdulatef <github@email.sendithere.co>
2024-11-03 05:56:28 +0300

    lets go

---
+++ web/src/pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,15 @@
+// import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const DashboardPage = () => {
+  return (
+    <>
+      <Metadata title="Dashboard" description="Dashboard page" />
+
+      <h1 className="bg-red-500">Dashboard</h1>
+      <p>Hello</p>
+    </>
+  )
+}
+
+export default DashboardPage
